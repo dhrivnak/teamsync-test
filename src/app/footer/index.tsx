c47d1f2b1b293fcd78da9d08dc085e6065dded4e
Daniel Hrivnak <dhrivnak@gmail.com>
2024-06-03 13:02:23 -0500

    Fixed images

--- src/app/footer/index.tsx
+++ src/app/footer/index.tsx
@@ -4,27 +4,73 @@ export const Footer = () => (
       <div className="frame-13895">
         <div className="logo-2">TeamSync</div>
         <div className="frame-13897">
-          <img src="instagram.svg" loading="lazy" width="29.01435661315918" height="29.01435661315918" alt="Instagram logo" className="vectors-wrapper-10" />
-          <img src="whatsapp.svg" loading="lazy" width="29.01435661315918" height="29.01435661315918" alt="Whatsapp logo" className="vectors-wrapper-10" />
-          <img src="twitter.svg" loading="lazy" width="29.01435661315918" height="29.01435661315918" alt="Twitter logo" className="vectors-wrapper-10" />
-          <img src="facebook.svg" loading="lazy" width="29.01435661315918" height="29.01435661315918" alt="Facebook logo" className="vectors-wrapper-10" />
+          <img
+            src="https://assets-global.website-files.com/642ad644644c1d199a312e4b/642dc4ca18ccd95fae059c22_Vectors-Wrapper.svg"
+            loading="lazy"
+            width="29.01435661315918"
+            height="29.01435661315918"
+            alt="Instagram logo"
+            className="vectors-wrapper-10"
+          />
+          <img
+            src="https://assets-global.website-files.com/642ad644644c1d199a312e4b/642dc4cb558a0c66d611e182_Vectors-Wrapper.svg"
+            loading="lazy"
+            width="29.01435661315918"
+            height="29.01435661315918"
+            alt="Whatsapp logo"
+            className="vectors-wrapper-10"
+          />
+          <img
+            src="https://assets-global.website-files.com/642ad644644c1d199a312e4b/642dc4ccc3503bd0b5e70831_Vectors-Wrapper.svg"
+            loading="lazy"
+            width="29.01435661315918"
+            height="29.01435661315918"
+            alt="Twitter logo"
+            className="vectors-wrapper-10"
+          />
+          <img
+            src="https://assets-global.website-files.com/642ad644644c1d199a312e4b/642dc4cc962ed90d4b8a8cf8_Vectors-Wrapper.svg"
+            loading="lazy"
+            width="29.01435661315918"
+            height="29.01435661315918"
+            alt="Facebook logo"
+            className="vectors-wrapper-10"
+          />
         </div>
       </div>
       <div className="frame-13898">
-        <a href="#" className="link-6">Features</a>
-        <a href="#" className="link-7">Resources</a>
-        <a href="#" className="link-7">Articles</a>
+        <a href="#" className="link-6">
+          Features
+        </a>
+        <a href="#" className="link-7">
+          Resources
+        </a>
+        <a href="#" className="link-7">
+          Articles
+        </a>
       </div>
       <div className="frame-13898">
-        <a href="#" className="link-7">FAQs</a>
-        <a href="#" className="link-7">Blogs</a>
-        <a href="#" className="link-7">Support</a>
+        <a href="#" className="link-7">
+          FAQs
+        </a>
+        <a href="#" className="link-7">
+          Blogs
+        </a>
+        <a href="#" className="link-7">
+          Support
+        </a>
       </div>
       <div className="frame-13899">
-        <a href="#" className="link-8">Careers</a>
-        <a href="#" className="link-8">Terms of Service</a>
-        <a href="#" className="link-8">Privacy Policy</a>
+        <a href="#" className="link-8">
+          Careers
+        </a>
+        <a href="#" className="link-8">
+          Terms of Service
+        </a>
+        <a href="#" className="link-8">
+          Privacy Policy
+        </a>
       </div>
     </div>
   </footer>
-)
+);
